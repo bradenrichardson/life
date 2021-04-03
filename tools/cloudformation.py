4565178e61abe207e6079ffc4f4b17a842419bb4
Braden Richardson <braden.richardson13@gmail.com>
2021-04-03 10:57:28 +0800

    continued custom resource development
    - lambda function can create an object in the s3 bucket
    - Custom resource can send a SUCCESS response on creation
    - Still testing deletion for both s3 and cfnresponse

--- tools/cloudformation.py
+++ tools/cloudformation.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(description="Deploy cloudformation stacks to AW
 parser.add_argument('--stack_name', type=str, required=True)
 parser.add_argument('--create', type=bool)
 parser.add_argument('--delete', type=bool)
+parser.add_argument('--update', type=bool)
 args = parser.parse_args()
 
 ## NOTE:
@@ -24,8 +25,16 @@ def delete_stack():
     cf_client.delete_stack(StackName=args.stack_name)
     print('Deleting stack: {}'.format(args.stack_name))
 
+def update_stack():
+    cf_template = open('infra.yml').read()
+    cf_client.update_stack(StackName=args.stack_name, TemplateBody=cf_template, Capabilities=[
+        'CAPABILITY_IAM',  'CAPABILITY_NAMED_IAM'])
+    print('Updating stack: {}'.format(args.stack_name))
+
 if __name__ == '__main__':
     if args.create:
         create_stack()
     if args.delete:
         delete_stack()
+    if args.update:
+        update_stack()
